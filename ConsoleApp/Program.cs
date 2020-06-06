5015c42a9c9e64b1331155cc33cdbb4ebfe5718d
AnnaLviv <annanastasiyalviv@gmail.com>
2020-06-06 12:24:31 +0200

    Rough first version of EF implementation with ConsoleApp as a sandbox and hardcoded connection string.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using EntityFrameworkCoreSample.Data;
+using EntityFrameworkCoreSample.Domain;
+using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+        private static void Main(string[] args)
+        {
+            //cheating approach for sandbox purposes
+            context.Database.EnsureCreated();
+            GetSamurais("Before adding:");
+            AddSamurai();
+            GetSamurais("After adding:");
+
+            Console.WriteLine("Press any key ...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Ramses" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais (string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text} Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+                Console.WriteLine(samurai.Name);
+        }
+    }
+}
